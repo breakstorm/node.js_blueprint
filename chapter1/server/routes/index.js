b5e52fc63480b5d5bda803e2c93f0f2d04dd7aee
김휘겸 <breakstorm00@gmail.com>
2017-10-10 16:55:05 +0900

    Update route argument pass to views page.

--- chapter1/server/routes/index.js
+++ chapter1/server/routes/index.js
@@ -5,19 +5,19 @@ router.get('/', function(req,res,next){
 	console.log("enter / ");
 	// res.sendFile('index.html', {root: __dirname + './../../public/HTML/'})
 	// res.render('index', {title: 'Login Page', message: req.flash('loginMessage')});
-	res.render('signin');
+	res.render('signin', {title : "Signin", message: req.flash('loginMessage')});
 })
 
 router.get('/signin', function(req, res, next){
-	res.render('signin');
+	res.render('signin', {title : "Signin", message: req.flash('loginMessage')});
 })
 
 router.get('/signup', function(req, res, next){
-	res.render('signup');
+	res.render('signup', {title: "Signup", message: req.flash('signupMessage')});
 })
 
 router.get('/profile', function(req, res, next){
-	res.render('profile');
+	res.render('profile', {title: "Profile", user: req.user, avatar: gravatar.url(req.user.email, {s: '100', r: 'x', d: 'retro'}, true)});
 })
 
 module.exports = router;
