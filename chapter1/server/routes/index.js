14aa5bbd3f03767a66c9946dd63e4861bbf6bd1a
김휘겸 <breakstorm00@gmail.com>
2017-10-10 15:54:54 +0900

    Modify route path to view pages

--- chapter1/server/routes/index.js
+++ chapter1/server/routes/index.js
@@ -3,8 +3,21 @@ var router = express.Router();
 
 router.get('/', function(req,res,next){
 	console.log("enter / ");
-	res.sendFile('index.html', {root: __dirname + './../../public/HTML/'})
+	// res.sendFile('index.html', {root: __dirname + './../../public/HTML/'})
+	// res.render('index', {title: 'Login Page', message: req.flash('loginMessage')});
+	res.render('signin');
 })
 
+router.get('/signin', function(req, res, next){
+	res.render('signin');
+})
+
+router.get('/signup', function(req, res, next){
+	res.render('signup');
+})
+
+router.get('/profile', function(req, res, next){
+	res.render('profile');
+})
 
 module.exports = router;
