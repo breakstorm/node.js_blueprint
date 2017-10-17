baaa7618765241f56cdd8c9b938683bbe73a21d0
김휘겸 <breakstorm00@gmail.com>
2017-10-17 18:14:59 +0900

    Update route pages file

--- chapter1/server/routes/index.js
+++ chapter1/server/routes/index.js
@@ -1,12 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var gravatar = require('gravatar');
 
 router.get('/', function(req, res, next) {
-	console.log("hi")
-	// res.render('index', {title: 'Express from server folder'});
-	res.render('index');
-
+	res.render('index', {title: 'Express from server folder'});
+	// res.render('index');
 });
+
+router.get('/login', function(req, res, next) {
+	res.render('login', {title: 'Login page', message: 'hello login'})
+})
+
+router.get('/signup', function(req, res, next) {
+	res.render('signup', {title: 'Signup page', message: 'hello signup'})
+})
+
+router.get('/profile', function(req, res, next) {
+	var testUser = {
+		local:{
+			name: 'aaa',
+			email: 'aaa@mail.com'
+		}
+	};
+	res.render('profile', {title: 'Profile page', avatar: 
+		gravatar.url('aaa@mail.com', {s: '100', r: 'x', d: 'retro'}, true), user: testUser})
+})
 
 
 module.exports = router;
