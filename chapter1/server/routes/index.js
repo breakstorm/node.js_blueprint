a1821abe7d11e33483a307862f3069e867097174
김휘겸 <breakstorm00@gmail.com>
2017-10-17 15:56:22 +0900

    Fix signin and signout function

--- chapter1/server/routes/index.js
+++ chapter1/server/routes/index.js
@@ -1,70 +1,53 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+// get gravatar icon from email
 var gravatar = require('gravatar');
 
-router.get('/', function(req,res,next){
-	console.log("enter / ");
-	// res.sendFile('index.html', {root: __dirname + './../../public/HTML/'})
-	// res.render('index', {title: 'Login Page', message: req.flash('loginMessage')});
-	res.render('signin', {title : "Signin", message: req.flash('loginMessage')});
-})
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express from server folder' });
+});
 
-router.get('/signin', function(req, res, next){
-	res.render('signin', {title : "Signin", message: req.flash('loginMessage')});
-})
-
-router.get('/signup', function(req, res, next){
-	res.render('signup', {title: "Signup", message: req.flash('signupMessage')});
-})
-
-router.get('/profile', isLoggedIn ,function(req, res, next){
-	res.render('profile', {title: "Profile", user: req.user, avatar: gravatar.url(req.user.email, {s: '100', r: 'x', d: 'retro'}, true)});
-	// var testUser = {
-	// 	name: "test",
-	// 	email: "test email"		
-	// }
-	// res.render('signin', {title: "profile", user: testUser});
-})
-
-router.get('/profile2', function(req, res, next) {
-	var testUser = {
-		name: "test",
-		email: "test email"		
-	}
-	// res.render('profile', {title: "profile", user: testUser});
-	res.render('profile', {title: "Profile", user: testUser, avatar: gravatar.url(req.user.email, {s: '100', r: 'x', d: 'retro'}, true)});
-})
-
-router.post('/login',
-	passport.authenticate('local-login', {
-	successRedirect: '/profile',
-	failureRedirect: '/signin',
-	failureFlash: true
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+    res.render('login', { title: 'Login Page', message: req.flash('loginMessage') });
+});
+/* POST login */
+router.post('/login', passport.authenticate('local-login', {
+    //Success go to Profile Page / Fail go to login page
+    successRedirect : '/profile',
+    failureRedirect : '/login',
+    failureFlash : true
 }));
 
-router.post('/signup',	passport.authenticate('local-signup', {
-	successRedirect: '/profile',
-	failureRedirect: '/signup',
-	failureFlash: true
+/* GET Signup */
+router.get('/signup', function(req, res) {
+    res.render('signup', { title: 'Signup Page', message: req.flash('signupMessage') });
+});
+/* POST Signup */
+router.post('/signup', passport.authenticate('local-signup', {
+    //Success go to Profile Page / Fail go to Signup page
+    successRedirect : '/profile',
+    failureRedirect : '/signup',
+    failureFlash : true
 }));
 
-function isLoggedIn(req, res, next){
-	console.log("enter function isLoggedIn")
-	if(req.isAuthenticate()){
-		console.log("success isLoggedIn")
-		return next();
-	}
-	
-	console.log("fail isLoggedIn")
-	res.redirect('/signin');
+/* GET Profile page. */
+router.get('/profile', isLoggedIn, function(req, res, next) {
+    res.render('profile', { title: 'Profile Page', user : req.user, avatar: gravatar.url(req.user.email ,  {s: '100', r: 'x', d: 'retro'}, true) });
+});
+
+/* check if user is logged in */
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+    res.redirect('/login');
 }
-
-router.get('/logout', function(req,res) {
-	req.logout();
-	res.redirect('/');
-})
-
-
+/* GET Logout Page */
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
 
 module.exports = router;
