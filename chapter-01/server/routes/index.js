b1d925e0df90ea8f5e90bc636477d096c0240789
김휘겸 <breakstorm00@gmail.com>
2017-10-24 16:21:36 +0900

    Update partials, pages file. index pages.

--- chapter-01/server/routes/index.js
+++ chapter-01/server/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+	res.render('login', {title: 'login', message: 'undefined'})
+})
+
 module.exports = router;
