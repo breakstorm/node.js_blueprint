6b8925d9f4fb52ad92113fa9c5ffe2fd7e566af5
김휘겸 <breakstorm00@gmail.com>
2017-10-24 16:58:51 +0900

    Update profile, signup pages

--- chapter-01/server/routes/index.js
+++ chapter-01/server/routes/index.js
@@ -7,7 +7,24 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', function(req, res, next) {
+	// res.render('login', {title: 'login', message: req.flash('loginMessage')})
 	res.render('login', {title: 'login', message: 'undefined'})
+})
+
+router.get('/signup', function(req, res, next) {
+	// res.render('login', {title: 'login', message: req.flash('loginMessage')})
+	res.render('signup', {title: 'signup', message: 'undefined'})
+})
+
+router.get('/profile', function(req, res, next) {
+	var user = {
+		local: {
+			name: "test name",
+			email: "test email"
+		}
+	}
+	// res.render('login', {title: 'login', message: req.flash('loginMessage')})
+	res.render('profile', {title: 'profile', message: 'undefined', user: user, avatar: 'undefined'})
 })
 
 module.exports = router;
