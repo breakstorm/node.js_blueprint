c273993241869523007e49a7022a19c3c4bd3bba
김휘겸 <breakstorm00@gmail.com>
2017-10-31 12:42:18 +0900

    Fix swig setting parameter

--- chapter-02/app.js
+++ chapter-02/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 //add hwigyum
-var swig = requrie('swig');
+var swig = require('swig');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -17,10 +17,10 @@ var app = express();
 // app.set('view engine', 'jade');
 
 // add hwigyum : template engine set
-//var swig = new swig.Swig();
-app.engine('views', swig,renderFile);
-app.set('view engine', 'html');
+var swig = new swig.Swig();
+app.engine('views', swig.renderFile);
 app.set('views', path.join(__dirname, 'views/pages'));
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
