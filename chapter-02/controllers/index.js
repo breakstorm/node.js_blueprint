3883ca2dd2a92542b6733e86b38b1ca607a2cd16
김휘겸 <breakstorm00@gmail.com>
2017-11-02 10:59:48 +0900

    Update page layout

--- chapter-02/controllers/index.js
+++ chapter-02/controllers/index.js
@@ -1,27 +1,44 @@
 
 // hwigyum add : index controller
-exports.show = function(req, res) {
-	res.render('index', {title: 'Express'});
-};
 
 exports.show = function(req, res) {
-	var topBands = [
+	// var topBands = [
+ //        {
+ //            name: 'Motorhead',
+ //            description: 'Rock and Roll Band',
+ //            album: 'http://s2.vagalume.com/motorhead/discografia/orgasmatron-W320.jpg',
+ //            year:'1986',
+ //        },
+ //        {
+ //            name: 'Judas Priest',
+ //            description: 'Heavy Metal band',
+ //            album: 'http://s2.vagalume.com/judas-priest/discografia/screaming-for-vengeance-W320.jpg',
+ //            year:'1982',
+ //        },
+ //        {
+ //            name: 'Ozzy Osbourne',
+ //            description: 'Heavy Metal Band',
+ //            album: 'http://s2.vagalume.com/ozzy-osbourne/discografia/diary-of-a-madman-W320.jpg',
+ //            year:'1981',
+ //        }
+ //    ];
+     var topBands = [
         {
             name: 'Motorhead',
             description: 'Rock and Roll Band',
-            album: 'http://s2.vagalume.com/motorhead/discografia/orgasmatron-W320.jpg',
+            album: null,
             year:'1986',
         },
         {
             name: 'Judas Priest',
             description: 'Heavy Metal band',
-            album: 'http://s2.vagalume.com/judas-priest/discografia/screaming-for-vengeance-W320.jpg',
+            album: null,
             year:'1982',
         },
         {
             name: 'Ozzy Osbourne',
             description: 'Heavy Metal Band',
-            album: 'http://s2.vagalume.com/ozzy-osbourne/discografia/diary-of-a-madman-W320.jpg',
+            album: null,
             year:'1981',
         }
     ];
