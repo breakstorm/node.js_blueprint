3d9c676f348d92a3f5999f55fa975e27181ff019
김휘겸 <breakstorm00@gmail.com>
2017-11-07 11:50:14 +0900

    Fix videoController and view page

--- chapter-03/server/controllers/images.js
+++ chapter-03/server/controllers/images.js
@@ -22,55 +22,68 @@ exports.show = function(req, res) {
 }
 
 exports.uploadImage = function(req, res) {
-	var src;
-	var dest;
-	var targetPath;
-	var targetName;
-	var tempPath = req.file.path;
-	console.log(req.file);
+    var src;
+    var dest;
+    var targetPath;
+    var targetName;
+    var tempPath = req.file.path;
+    console.log(req.file);
+    //get the mime type of the file
+    var type = mime.lookup(req.file.mimetype);
+    // get file extension
+    var extension = req.file.path.split(/[. ]+/).pop();
+    // check support file types
+    if (IMAGE_TYPES.indexOf(type) == -1) {
+        return res.status(415).send('Supported image formats: jpeg, jpg, jpe, png.');
+    }
+    // Set new path to images
+    targetPath = './public/images/' + req.file.originalname;
+    // using read stream API to read file
+    src = fs.createReadStream(tempPath);
+    // using a write stream API to write file
+    dest = fs.createWriteStream(targetPath);
+    src.pipe(dest);
 
-	var type = mime.lookup(req.file.mimetype);
-	var extension = req.file.path.split(/[. ]+/).pop();
+    // Show error
+    src.on('error', function(err) {
+        if (err) {
+            return res.status(500).send({
+                message: error
+            });
+        }
+    });
 
-	if(VIDEO_TYPES.indexOf(type) == -1) {
-		return res.status(415).send('Supported video formats: mp4, webm, ogg, ogv');
-	}
-	targetPath = './public/videos/' + req.file.originalname;
-	src = fs.createReadStream(tempPath);
-	dest = fs.createWriteStream(targetPath);
-	src.pipe(dest);
-	src.on('error', function(error) {
-		if(error){
-			return res.status(500).send({
-				message: error
-			});
-		}
-	})
+    // Save file process
+    src.on('end', function() {
+        // create a new instance of the Images model with request body
+        var image = new Images(req.body);
+        // Set the image file name
+        image.imageName = req.file.originalname;
+        // Set current user (id)
+        image.user = req.user;
+        // save the data received
+        image.save(function(error) {
+            if (error) {
+                return res.status(400).send({
+                    message: error
+                });
+            }
+        });
+        // remove from temp folder
+        fs.unlink(tempPath, function(err) {
+            if (err) {
+                return res.status(500).send('Woh, something bad happened here');
+            }
+            // Redirect to galley's page
+            res.redirect('images-gallery');
 
-	src.on('end', function() {
-		var video = new Videos(req.body);
-		video.videoName = req.file.originalname;
-		video.user = req.user;
-		video.save(function(error) {
-			return res.status(400).send({
-				message: error
-			});
-		})
+        });
+    });
+};
 
-		fs.unlink(tempPath, function(err) {
-			if(err) {
-				return res.status(500).send({
-					message: error
-				})
-			}
-
-			res.redirect('videos');
-		})
-	})
-}
-
+// Images authorization middleware
 exports.hasAuthorization = function(req, res, next) {
-	if(req.isAuthenticated())
-		return next();
-	res.redirect('/login');
-}
+    if (req.isAuthenticated())
+    return next();
+    res.redirect('/login');
+};
